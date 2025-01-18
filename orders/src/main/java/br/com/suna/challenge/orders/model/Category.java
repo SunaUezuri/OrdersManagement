8794f1fb35e9c4fe41e2680919715ccea1b59ab9
SunaUezuri <wesley.jane.santos@gmail.com>
2025-01-17 22:08:24 -0300

    Class Category created

---
+++ orders/src/main/java/br/com/suna/challenge/orders/model/Category.java
@@ -0,0 +1,41 @@
+package br.com.suna.challenge.orders.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "t_cll_category")
+public class Category {
+
+    //Class attributes
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    //Class constructors
+    public Category() {}
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    //Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
